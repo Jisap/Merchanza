e8089efe1d4a7c6e8a9803ad929ee0749d4c97f7
Jorge Matos <jisapjisap@gmail.com>
2024-05-14 15:32:30 +0200

    swiper

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -3,6 +3,7 @@ import LATEST from '../assets/latest'
 import { Link } from 'react-router-dom'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
+import { RiShoppingCart2Line } from 'react-icons/ri'
 
 const RelatedProducts = () => {
   return (
@@ -10,12 +11,44 @@ const RelatedProducts = () => {
       <h4>Related Products</h4>
       {/* container */}
       <div>
-        <Swiper>
+        <Swiper
+          breakpoints={{
+            600: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+            },
+            1200: {
+              slidesPerView: 4,
+              spaceBetween: 30,
+            },
+          }}
+        >
           {
             LATEST.map((item, i) => (
               <SwiperSlide key={i}>
                 <Link to={''}>
                   <img src={item.image} alt='' />
+                  
+                  <div>
+                    <h4>
+                      {item.name}
+                    </h4>
+                    <p>
+                      Distinctio atque harum omnis at iure libero est. Corporis eum sint in. Qui est veniam odit ut.
+                    </p>
+                    <div>
+                      <div>
+                        <span>${item.new_price}.00</span>
+                        <span>${item.old_price}.00</span>
+                      </div>
+                      <RiShoppingCart2Line />
+                    </div>
+                  </div>
+
                 </Link>
               </SwiperSlide>
             ))
