8e642bc4443bfc48892fbf45c720b77f7cc2ac03
Jorge Matos <jisapjisap@gmail.com>
2024-05-14 15:20:45 +0200

    Related products

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import LATEST from '../assets/latest'
+import { Link } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
 
 const RelatedProducts = () => {
   return (
-    <div>RelatedProducts</div>
+    <div>
+      <h4>Related Products</h4>
+      {/* container */}
+      <div>
+        <Swiper>
+          {
+            LATEST.map((item, i) => (
+              <SwiperSlide key={i}>
+                <Link to={''}>
+                  <img src={item.image} alt='' />
+                </Link>
+              </SwiperSlide>
+            ))
+          }
+        </Swiper>
+      </div>
+    </div>
   )
 }
 
