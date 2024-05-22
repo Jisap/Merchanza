1b3d745039af99a372a357cea59e4a1c450d856c
Jorge Matos <jisapjisap@gmail.com>
2024-05-22 14:45:02 +0200

    productDescription

--- frontend/src/components/ProductDescription.jsx
+++ frontend/src/components/ProductDescription.jsx
@@ -14,7 +14,7 @@ const ProductDescription = () => {
           ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
           laboris nisi ut aliquip ex ea commodo consequat.
         </p>
-        <p className="text-sm mt-5">
+        <p className="text-sm mt-2">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
           laboris nisi ut aliquip ex ea commodo consequat.
