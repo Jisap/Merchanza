a42e0bdccece30dc8f081e979eda97acb112a08d
Jorge Matos <jisapjisap@gmail.com>
2024-05-30 12:57:09 +0200

    admin structure

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Navbar from './components/Navbar'
+import Admin from './pages/Admin'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <h1 className="font-bold">Admin</h1>
+    <main classname='bg-primary text-tertiary'>
+      <div classname='mx-auto max-w-[1500px]'>
+        <Navbar />
+        <Admin />
       </div>
-        
-    </>
+    </main>
   )
 }
 
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+import profileImg from '../assets/profile.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <div>
+          <img src={logo} alt='logoIcon' />
+        </div>
+        <div>Admin Panel</div>
+        <div>
+          <img src={profileImg} alt='' />
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
