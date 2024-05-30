31248cbbd0cfff23c460b78e29f2212f2ff987e8
Jorge Matos <jisapjisap@gmail.com>
2024-05-30 11:51:53 +0200

    addproduct

--- backend/index.js
+++ backend/index.js
@@ -26,23 +26,86 @@ app.get("/", (req, res) => {
 
 // image storage engine
 
-const storage = multer.diskStorage({
+const storage = multer.diskStorage({                                                      // Configuración el almacenamiento en disco para los archivos subidos.
   destination: './upload/images',
   filename: (req, file, cb) => {
     return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
   }
 });
 
-const upload = multer({storage:storage});
+const upload = multer({ storage: storage });                             // Se crea una instancia de multer utilizando la configuración de almacenamiento definida previamente.
 
 // Crating endpoint for images
-app.use("images", express.static("upload/images"))
-app.post("/upload", upload.single("product"), (req, res) => {
+app.use("/images", express.static("upload/images"))                      // Se configura una ruta estática para acceder a los archivos subidos http://localhost:4000/images
+app.post("/upload", upload.single("product"), (req, res) => {            // endpoint POST en /upload // upload.single("product"): Middleware de multer para manejar la subida de un solo archivo con el campo de formulario llamado product. 
   res.json({
     success: 1,
     image_url: `http://localhost:${port}/images/${req.file.filename}`
   })
+});
+
+// Schema for creating products
+const Product = mongoose.model("Product",  {
+  id:{
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true
+  },
+});
+
+app.post("/addproduct", async(req, res) => {
+
+  let products = await Product.find({})
+  let id;
+
+  if(products.length > 0){
+    let last_product_array = products.slice(-1);
+    let last_product = last_product_array[0];
+    id = last_product.id+1
+  }else{
+    id = 1;
+  }
+
+  const product = new Product({
+    id: id,
+    name: req.body.name,
+    image: req.body.image,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+  })
+  console.log(product)
+  await product.save();
+  console.log("Saved")
+  res.json({
+    success: true,
+    name: req.body.name
+  })
 })
+
 
 app.listen(port, (error) => {
   if(!error){
