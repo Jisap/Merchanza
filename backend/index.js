231bf40bb44117c2273f98f220be5a9ef3b2d840
Jorge Matos <jisapjisap@gmail.com>
2024-05-30 12:10:07 +0200

    allproducts

--- backend/index.js
+++ backend/index.js
@@ -104,6 +104,23 @@ app.post("/addproduct", async(req, res) => {
     success: true,
     name: req.body.name
   })
+});
+
+// Creating API for deleting products
+app.post("/removeproduct", async(req, res) => {
+  await Product.findOneAndDelete({id:req.body.id});
+  console.log("Removed")
+  res.json({
+    success: true,
+    name: req.body.name
+  })
+});
+
+//Creating API for getting all products
+app.get("/allproducts", async(req, res) => {
+  let products = await Product.find({})
+  console.log("All products fetched")
+  res.send(products)
 })
 
 
